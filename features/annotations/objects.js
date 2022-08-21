7b65f0542bcf9b324549dedfc0462762424608d1
Bhavleen Singh <singhbhavleen3@gmail.com>
2022-08-21 20:25:20 +0530

    Objects annotations in TS

---
+++ features/annotations/objects.js
@@ -0,0 +1,9 @@
+var todaysWeather = {
+    date: new Date(),
+    weather: "sunny"
+};
+var logWeather = function (todaysWeather) {
+    console.log(todaysWeather.date);
+    console.log(todaysWeather.weather);
+};
+logWeather(todaysWeather);
