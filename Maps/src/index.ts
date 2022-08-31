8563fe3f5dde045a88908933f8c6f823b6ad48fb
Bhavleen Singh <singhbhavleen3@gmail.com>
2022-08-31 12:16:44 +0530

    Starting a project with Typescript

--- Maps/src/index.ts
+++ Maps/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hey Bhavleen");
+/// <reference types="@types/google.maps" />
+
+// import { Company } from "./Company";
+// import { User } from "./User";
+
+// const user = new User();
+
+// const company = new Company();
+
+// console.log(user);
+// console.log(company);
+
+if (document.getElementById("map")) {
+
+
+    new google.maps.Map(document.getElementById("map"), {
+        zoom: 1,
+        center: {
+            lat: 0,
+            lng: 0,
+        },
+    });
+
+}
